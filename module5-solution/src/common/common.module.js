91530f10d56c65458164c5977fe54cee8ad48c64
Bivisha Karki <bivisha2056@gmail.com>
2020-09-20 23:41:02 +0545

    api updated

--- module5-solution/src/common/common.module.js
+++ module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://shobhitsri129.herokuapp.com')
+.constant('ApiPath', 'https://bvsa-coursera-angular-course5.herokuapp.com')
 .config(config);
 
 config.$inject = ['$httpProvider'];
